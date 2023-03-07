2c4932284e68ec2e3f31d4154346610f5304c4e3
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-03-07 12:19:32 -0400

    Team matches

--- commands/matches.js
+++ commands/matches.js
@@ -9,20 +9,20 @@ const createEmbed = (team) => {
     .setTitle(team.home_team_en)
     //.setURL(`https://en.wikipedia.org/wiki/${team.name.common}`)
     .setDescription('Shows team matches')
-    //.setThumbnail(`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`)
+    .setThumbnail(team.home_flag)
     .addFields(
       { name: 'Local date', value: team.local_date },
       { name: 'Match Day', value: team.matchday, inline: true },
       { name: 'Home team', value: team.home_team_en, inline: true },
       { name: 'Away team', value: team.away_team_en, inline: true },
-      { name: 'Home Score', value: team.home_score, inline: true },
-      { name: 'Away Score', value: team.away_score, inline: true },
+      { name: 'Home Score', value: `${team.home_score}`, inline: true },
+      { name: 'Away Score', value: `${team.away_score}`, inline: true },
       //{ name: 'Temperature', value: `${weather.main.temp} C`, inline: true },
       //{ name: 'Weather', value: `${weather.weather[0].description[0].toUpperCase() + weather.weather[0].description.substring(1)}`, inline: true },
     )
     //.addFields({ name: 'Time-zone', value: team.timezones[0], inline: true })
-    .setImage(team.home_flag)
-    .setImage(team.away_flag);
+    //.setImage(team.home_flag);
+    //.setImage(team.away_flag);
     //.setFooter({ text: 'Some climate here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
   return exampleEmbed;
@@ -44,9 +44,8 @@ module.exports = {
         .setRequired(true)),
   async execute(interaction) {
 
-    // await interaction.deferReply();
-    // //await wait(4000);
-    // await interaction.editReply('Pong!');
+
+    await interaction.deferReply();
 
     try {
       const team = interaction.options.getString('equipo');
@@ -61,32 +60,32 @@ module.exports = {
       const { data:response } = await axios.get('http://api.cup2022.ir/api/v1/match', {
         headers: { 'Authorization' : `Bearer ${token}` }
       });
-      const t = response.data.find(equipo => equipo.name_en === team);
-      console.log(response.data.filter(equipo => equipo.home_team_en === team || equipo.away_team_en === team));
+      //const t = response.data.find(equipo => equipo.name_en === team);
+      const matches = response.data.filter(equipo => equipo.home_team_en === team || equipo.away_team_en === team);
 
-      if (!t) {
-        return await interaction.reply('El pais no participo en el mundial Qatar22');
+      if (matches.length === 0) {
+        return await interaction.editReply('El pais no participo en el mundial Qatar22');
       }
 
       console.log(response);
-      const embed = createEmbed(t);
-      await interaction.reply( { embeds: [embed] } );
+      const embed = createEmbed(matches[2]);
+      await interaction.editReply( { embeds: [embed] } );
     } catch (error) {
       //ver errores de la API
       console.log(error?.response?.data?.message);
       if (error?.response?.data?.message) {
-        return await interaction.reply('User validation failed: email: Please provide a valid email, password: Path `password` (`00`) is shorter than the minimum allowed length (8)., passwordConfirm: Path `passwordConfirm` (`00`) is shorter than the minimum allowed length (8)');
+        return await interaction.editReply('User validation failed: email: Please provide a valid email, password: Path `password` (`00`) is shorter than the minimum allowed length (8)., passwordConfirm: Path `passwordConfirm` (`00`) is shorter than the minimum allowed length (8)');
       }
 
       //otros errores
       console.log(error.message);
       switch (error.message) {
       case 'Cannot read properties of undefined (reading name_en)':
-        return await interaction.reply('El pais no participo en el mundial');
+        return await interaction.editReply('El pais no participo en el mundial');
       case 'UNIQUE constraint failed: users.email':
-        return await interaction.reply('Tu email ya se encuentra registrado');
+        return await interaction.editReply('Tu email ya se encuentra registrado');
       default:
-        return await interaction.reply('Hubo un error!');
+        return await interaction.editReply('Hubo un error!');
       }
 
     }
